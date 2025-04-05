d7f652d9db358556179d60fd5bcf7daadc49c08b
William Aguas <148153196+WILLIAMAGUAS@users.noreply.github.com>
2025-04-05 03:45:34 +0000

    primer avance

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let number =["A","2","3","4","5","6","8","9","10","J","Q","K"]
+  let numbercart =number[Math.floor(Math.random()*number.length)]
+  
+  document.getElementById("center").innerHTML=numbercart
+  let pinta = ["♦", "♥", "♠", "♣"]
+  let pintacart = pinta[Math.floor(Math.random()*pinta.length)]
+  
+  document.querySelectorAll(".figura").forEach(figure => figure.innerHTML = pintacart)
+
+
+
   //write your code here
   console.log("Hello Rigo from the console!");
 };
